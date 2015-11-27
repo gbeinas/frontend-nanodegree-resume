6296f727fee537007a13ff5fa7df52fdd4477277
George Beinas <gbeinas@di.uoa.gr>
2015-11-27 11:41:04 +0200

    Changes in helper.js file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*var awesomeThoughts = "I am George and I am awesome";
+funthoughts = awesomeThoughts.replace("awesome", "FUN");
+
+$("#main").append(funthoughts);*/
+
+/*formattedName = HTMLheaderName.replace("%data%","George Beinas");
+$("#header").append(formattedName);*/
+
+// console.log(funthoughts);
+
+ /*var email = "gbeinas@di.uoa.gr"
+ var newemail = email.replace("di", "gmail");
+
+ console.log(email);
+ console.log(newemail);*/
+
